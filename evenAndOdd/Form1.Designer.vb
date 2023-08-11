d70a69d3d59c4ff9f91717c534eac5e444ca79b8
HewTheBox <109417136+HewTheBox@users.noreply.github.com>
2023-08-11 04:53:19 -1200

    Add files via upload

---
+++ evenAndOdd/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.lstEven = New System.Windows.Forms.ListBox()
+        Me.lstOdd = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(291, 445)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(192, 53)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 16
+        Me.lstDisplay.Location = New System.Drawing.Point(110, 89)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(184, 196)
+        Me.lstDisplay.TabIndex = 3
+        '
+        'lstEven
+        '
+        Me.lstEven.FormattingEnabled = True
+        Me.lstEven.ItemHeight = 16
+        Me.lstEven.Location = New System.Drawing.Point(651, 89)
+        Me.lstEven.Name = "lstEven"
+        Me.lstEven.Size = New System.Drawing.Size(184, 196)
+        Me.lstEven.TabIndex = 4
+        '
+        'lstOdd
+        '
+        Me.lstOdd.FormattingEnabled = True
+        Me.lstOdd.ItemHeight = 16
+        Me.lstOdd.Location = New System.Drawing.Point(379, 89)
+        Me.lstOdd.Name = "lstOdd"
+        Me.lstOdd.Size = New System.Drawing.Size(184, 196)
+        Me.lstOdd.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(116, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Original Array"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(682, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Even Array"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(400, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Odd Array"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(916, 579)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstOdd)
+        Me.Controls.Add(Me.lstEven)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lstDisplay As ListBox
+    Friend WithEvents lstEven As ListBox
+    Friend WithEvents lstOdd As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ evenAndOdd/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim number As String = " "
+
+        Dim numbers() As Integer = Array.ConvertAll(InputBox("Enter a set of numbers seperated by space: ", "input numbers").Split(number), Function(x) CInt(x))
+        GetOddNumbers(numbers)
+        GetEvenNumbers(numbers)
+
+        Dim size = numbers.Length - 1
+        For i As Integer = 0 To size Step 1
+            lstDisplay.Items.Add(numbers(i))
+        Next
+    End Sub
+    Function GetEvenNumbers(ByVal numbers() As Integer) As Integer()
+        Dim evenNumbers() As Integer
+
+        For Each num As Integer In numbers
+            If num Mod 2 = 0 Then
+                'num is even, add it to the evenNumbers array
+                Dim index As Integer = 10
+                ReDim Preserve evenNumbers(index)
+                evenNumbers(index - 1) = num
+                lstEven.Items.Add(num)
+            End If
+        Next
+        Return evenNumbers
+    End Function
+    Function GetOddNumbers(ByVal numbers() As Integer) As Integer()
+        Dim OddNumbers() As Integer
+
+        For Each num As Integer In numbers
+            If num Mod 3 = 0 Then
+                'num is even, add it to the evenNumbers array
+                Dim index As Integer = 10
+                ReDim Preserve OddNumbers(index)
+                OddNumbers(index - 1) = num
+                lstOdd.Items.Add(num)
+            End If
+        Next
+        Return OddNumbers
+    End Function
+
+End Class
+
